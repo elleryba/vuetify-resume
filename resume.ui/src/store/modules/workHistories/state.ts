9f478ebe0b6e8fd06b5c729d2e1525a03c6dfd2c
eller <elleryba@icloud.com>
2022-03-13 11:07:27 -0400

    - offloaded some stuff - need to do further research on setting up my vuex store

--- resume.ui/src/store/modules/workHistories/state.ts
+++
@@ -1,7 +0,0 @@
-import { WorkHistoryInterface, WorkHistoryStoreInterface } from '@/store/modules/workHistories/types'
-
-export const state: WorkHistoryStoreInterface = {
-  items: [] as Array<WorkHistoryInterface>,
-}
-
-export default state
