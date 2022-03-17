7a58478a9831b5f0730e1690ec303db5eacdf77b
eller <elleryba@icloud.com>
2022-03-17 13:39:08 -0400

    - update PersonalInfo component data

--- resume.ui/src/components/PersonalInfo.vue
+++ resume.ui/src/components/PersonalInfo.vue
@@ -9,20 +9,19 @@
           </v-row>
           <v-row class="my-2 mx-0">
             <div align="left" class="text-subtitle-3">
-              In my personal time I am playing through the
+              <p>In my personal time I am playing through the
               <a
                 href="https://www.ea.com/games/mass-effect/mass-effect-legendary-edition"
                 target="_blank"
               >
-                Mass Effect
-              </a>
-              trilogy via
+                Mass Effect</a> trilogy via
               <a
                 href="https://www.xbox.com/en-US/games/store/xbox-game-pass-ultimate/cfq7ttc0khs0"
                 target="_blank"
-                >Xbox Game Pass</a
-              >. I am also working on this resume project, which I hope is
-              obvious.
+              >
+                Xbox Game Pass</a>. Happy to report I have finished the first game, 
+                and I am nearing the end of the second.</p>
+               <p>I am also working on this resume project, which I hope is obvious.</p>
             </div>
           </v-row>
         </v-card-text>
